88ac28eb38e2a0a9ab5a6d183ca7162913b40118
ballack <rodrigue-dev>
2023-07-08 09:07:57 +0100

    second init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {LOCALE_ID, NgModule} from '@angular/core';
+import {BrowserModule, Title} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgxWebstorageModule} from "ngx-webstorage";
+import {httpInterceptorProviders} from "./core/interceptor";
+import {MainComponent} from "./components/main/main.component";
+import {NavbarComponent} from "./components/navbar/navbar.component";
+import {ErrorComponent} from "./components/error/error.component";
+import {ActiveMenuDirective} from "./components/navbar/active-menu.directive";
+import {FooterComponent} from "./components/footer/footer.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxWebstorageModule.forRoot({ prefix: 'jhi', separator: '-', caseSensitive: true }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    Title,
+    { provide: LOCALE_ID, useValue: 'fr' },
+    httpInterceptorProviders,
+  ],
+  declarations: [MainComponent, NavbarComponent, ErrorComponent, ActiveMenuDirective, FooterComponent],
+  bootstrap: [MainComponent]
 })
 export class AppModule { }
---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'okoho-footer',
+  templateUrl: './footer.component.html',
+})
+export class FooterComponent {}
