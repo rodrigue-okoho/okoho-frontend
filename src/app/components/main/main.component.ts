df62f116e77f9e1a8ada6cdc7f44988d88380821
ballack <rodrigue-dev>
2023-07-08 09:11:48 +0100

    second init

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit, RendererFactory2, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router, ActivatedRouteSnapshot, NavigationEnd } from '@angular/router';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
-import * as dayjs from 'dayjs';
 
-import { AccountService } from 'app/core/auth/account.service';
 
 @Component({
   selector: 'okoho-main',
